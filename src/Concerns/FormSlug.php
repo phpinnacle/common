e09f8600a17a3a4fcb189644e09d25e96608de9c
Anton Shabouta <zloyusr@gmail.com>
2026-09-05 23:51:39 +0300

    [common+16]: Refine type contracts for PHPStan level 7

--- src/Concerns/FormSlug.php
+++ src/Concerns/FormSlug.php
@@ -2,6 +2,7 @@
 
 namespace PHPinnacle\Common\Concerns;
 
+use Closure;
 use Filament\Schemas\Components\Utilities\Get;
 use Filament\Schemas\Components\Utilities\Set;
 use Filament\Support\Enums\Operation;
@@ -9,7 +10,7 @@ use Illuminate\Support\Str;
 
 class FormSlug
 {
-    public static function make(string $field = 'slug', ?Operation $only = Operation::Create): callable
+    public static function make(string $field = 'slug', ?Operation $only = Operation::Create): Closure
     {
         return function (Get $get, Set $set, ?string $old, ?string $state, string $operation) use ($field, $only) {
             if ($only !== null && $operation !== $only->value) {
